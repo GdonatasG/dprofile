07828c9675d6dcccc12889c45b7e486a097183d4
GdonatasG <zitkusd@gmail.com>
2023-07-18 15:27:15 +0300

    Add temporary buttons for about-me feature nested navigation

--- features/about-me/src/androidMain/kotlin/com/donatas/dprofile/features/aboutme/AboutMeScreen.kt
+++ features/about-me/src/androidMain/kotlin/com/donatas/dprofile/features/aboutme/AboutMeScreen.kt
@@ -1,6 +1,11 @@
 package com.donatas.dprofile.features.aboutme
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 import com.donatas.dprofile.compose.components.layout.AppScaffold
 import com.donatas.dprofile.feature.Screen
 import com.donatas.dprofile.features.aboutme.presentation.AboutMeViewModel
@@ -12,7 +17,40 @@ actual class AboutMeScreen actual constructor() : Screen {
     override fun Compose() {
         val viewModel: AboutMeViewModel = getViewModel<AboutMeViewModel>()
 
-        AppScaffold {
+        AppScaffold(
+            tabBar = {
+                Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                    ElevatedButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Text(text = "Exp")
+                    }
+                    ElevatedButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Text(text = "Edu")
+                    }
+                    ElevatedButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Text(text = "Skills")
+                    }
+                    ElevatedButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Text(text = "Road")
+                    }
+                }
+            }
+        ) {
             AboutMeView(viewModel)
         }
     }
