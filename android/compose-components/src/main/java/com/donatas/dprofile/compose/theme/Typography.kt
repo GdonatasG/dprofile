f214116318bdc706eee7d98a43af5bb689d5d603
GdonatasG <zitkusd@gmail.com>
2023-07-19 16:11:20 +0300

    Create typography

---
+++ android/compose-components/src/main/java/com/donatas/dprofile/compose/theme/Typography.kt
@@ -0,0 +1,32 @@
+package com.donatas.dprofile.compose.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+private val DefaultTextStyle = TextStyle(
+    fontFamily = SpaceGrotesk,
+    fontWeight = FontWeight.W300,
+    fontSize = 16.sp,
+    lineHeight = 24.sp,
+    letterSpacing = 0.sp
+)
+
+val AppTypography = Typography(
+    displayLarge = DefaultTextStyle.copy(fontSize = 57.sp, lineHeight = 64.sp),
+    displayMedium = DefaultTextStyle.copy(fontSize = 45.sp, lineHeight = 52.sp),
+    displaySmall = DefaultTextStyle.copy(fontSize = 36.sp, lineHeight = 44.sp),
+    headlineLarge = DefaultTextStyle.copy(fontSize = 32.sp, lineHeight = 40.sp),
+    headlineMedium = DefaultTextStyle.copy(fontSize = 28.sp, lineHeight = 36.sp),
+    headlineSmall = DefaultTextStyle.copy(fontSize = 24.sp, lineHeight = 32.sp),
+    titleLarge = DefaultTextStyle.copy(fontSize = 22.sp, lineHeight = 28.sp),
+    titleMedium = DefaultTextStyle.copy(fontSize = 16.sp, lineHeight = 24.sp),
+    titleSmall = DefaultTextStyle.copy(fontSize = 13.sp, lineHeight = 20.sp),
+    bodyLarge = DefaultTextStyle.copy(fontSize = 16.sp, lineHeight = 24.sp),
+    bodyMedium = DefaultTextStyle.copy(fontSize = 14.sp, lineHeight = 20.sp),
+    bodySmall = DefaultTextStyle.copy(fontSize = 12.sp, lineHeight = 16.sp),
+    labelLarge = DefaultTextStyle.copy(fontSize = 14.sp, lineHeight = 20.sp),
+    labelMedium = DefaultTextStyle.copy(fontSize = 12.sp, lineHeight = 16.sp),
+    labelSmall = DefaultTextStyle.copy(fontSize = 11.sp, lineHeight = 16.sp)
+)
