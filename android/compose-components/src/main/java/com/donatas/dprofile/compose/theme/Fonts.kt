02ca6b5c18c3943649c739a3efde5952d21aab36
GdonatasG <zitkusd@gmail.com>
2023-07-19 13:32:34 +0300

    Add font

---
+++ android/compose-components/src/main/java/com/donatas/dprofile/compose/theme/Fonts.kt
@@ -0,0 +1,11 @@
+package com.donatas.dprofile.compose.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.donatas.dprofile.compose.components.R
+
+val OpenSans = FontFamily(
+    Font(R.font.opensans_regular, FontWeight(400)),
+    Font(R.font.opensans_semibold, FontWeight(600))
+)
